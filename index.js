934dfb417b7dff05c52a6e261a3ef9b93f38a0bd
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-06-11 03:08:56 +0600

    added a classes completed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8fciuv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,7 @@ async function run() {
 
     const allClassesCollection = client.db("summerCamp").collection("allClasses");
     const instractorsCollection = client.db("summerCamp").collection("instractors");
+    const classesCollection = client.db("summerCamp").collection("classes");
 
 
     // instractors collection
@@ -47,7 +48,30 @@ async function run() {
     })
 
 
+    // classes collection
+    app.get('/classes', async(req, res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query =  { email: email};
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
 
+    app.post('/classes', async(req, res) =>{
+      const item = req.body;
+      console.log(item);
+      const result = await classesCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/classes/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    })
     
 
     // Send a ping to confirm a successful connection
